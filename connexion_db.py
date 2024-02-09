d930b21631b4f0ae5f0a55c8368e7198cf6fab9e
kstru <kylian.strub@icloud.com>
2024-02-09 16:14:58 +0100

    test

--- connexion_db.py
+++ connexion_db.py
@@ -7,9 +7,9 @@ def get_db():
     if 'db' not in g:
         g.db = pymysql.connect(
             host="localhost",  # "serveurmysql" sur les machines de l'IUT
-            user="tcarrara",
-            password="230905",
-            database="BDD_tcarrara",
+            user="kstrub",
+            password="mdp",
+            database="BDD_kstrub",
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
