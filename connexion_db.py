2b3cf5fe9d18dfe4b2a69bd12fb7049b3d79560a
timcarrara <carrara.tim85@gmail.com>
2024-02-15 11:25:25 +0100

    nouveau

--- connexion_db.py
+++ connexion_db.py
@@ -7,9 +7,9 @@ def get_db():
     if 'db' not in g:
         g.db = pymysql.connect(
             host="localhost",  # "serveurmysql" sur les machines de l'IUT
-            user="kstrub",
-            password="mdp",
-            database="site_e-commerce_velo",
+            user="SAE_commun",
+            password="123456789",
+            database="e_commerce",
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
