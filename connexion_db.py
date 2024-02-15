33aefba8de7c780fe6e41c9695a589f6d01f493f
timcarrara <carrara.tim85@gmail.com>
2024-02-15 16:15:19 +0100

    nouveau

--- connexion_db.py
+++ connexion_db.py
@@ -1,15 +1,19 @@
 from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
 
 import pymysql.cursors
+import os
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('~/Site_e-commerce_velo')
+load_dotenv(os.path.join(project_folder, '.env'))
 
 def get_db():
     db = getattr(g, '_database', None)
     if 'db' not in g:
         g.db = pymysql.connect(
-            host="localhost",  # "serveurmysql" sur les machines de l'IUT
-            user="SAE_commun",
-            password="123456789",
-            database="e_commerce",
+            host=os.environ.get("HOST"),
+            user=os.environ.get("LOGIN"),
+            password=os.environ.get("PASSWORD"),
+            database=os.environ.get("DATABASE"),
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
