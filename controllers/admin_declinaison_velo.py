b574621e31e3d8bd2481b11c42148a740b5b46b3
timcarrara <carrara.tim85@gmail.com>
2024-03-31 15:42:25 +0200

    Update

--- controllers/admin_declinaison_velo.py
+++ controllers/admin_declinaison_velo.py
@@ -27,6 +27,7 @@ def add_declinaison_velo():
     mycursor.execute(sql, (id_velo,))
     d_taille_uniq = mycursor.fetchone()
     if d_taille_uniq is not None:
+        tailles.pop(0)
         taille_id = d_taille_uniq.get('taille_id')
         if taille_id == 1:
             d_taille_uniq = 1
@@ -35,6 +36,7 @@ def add_declinaison_velo():
     mycursor.execute(sql, (id_velo,))
     d_couleur_uniq = mycursor.fetchone()
     if d_couleur_uniq is not None:
+        couleurs.pop(0)
         couleur_id = d_couleur_uniq.get('couleur_id')
         if couleur_id == 1:
             d_couleur_uniq = 1
@@ -53,10 +55,13 @@ def valid_add_declinaison_velo():
              WHERE velo_id = %s AND taille_id = %s AND couleur_id = %s'''
     mycursor.execute(sql, tuple_select)
     declinaisonpresent = mycursor.fetchall()
-    if declinaisonpresent is None:
+    print(declinaisonpresent)
+    if not declinaisonpresent:
         tuple_insert = (stock_declinaison, id_velo, taille, couleur)
         sql = '''INSERT INTO declinaison_velo(id_declinaison_velo, stock, velo_id, taille_id, couleur_id) VALUES (NULL, %s, %s, %s, %s)'''
         mycursor.execute(sql, tuple_insert)
+        message = u'declinaison_article ajouté , velo_id: ' + id_velo + ' - stock : ' + stock_declinaison + ' - couleur : ' + couleur + ' - taille : ' + taille
+        flash(message, 'alert-success')
     else:
         tuple_update = (stock_declinaison, id_velo, taille, couleur)
         sql = '''UPDATE declinaison_velo SET stock = %s
@@ -67,16 +72,50 @@ def valid_add_declinaison_velo():
     return redirect('/admin/velo/edit?id_velo=' + id_velo)
 
 
+@admin_declinaison_velo.route('/admin/declinaison_velo/edit', methods=['GET'])
+def edit_declinaison_velo():
+    id_declinaison_velo = request.args.get('id_declinaison_velo')
+    mycursor = get_db().cursor()
+    sql = '''SELECT * FROM declinaison_velo
+             JOIN velo ON declinaison_velo.velo_id = velo.id_velo
+             WHERE id_declinaison_velo = %s;'''
+    mycursor.execute(sql, (id_declinaison_velo,))
+    declinaison_velo = mycursor.fetchone()
+    sql = '''SELECT id_couleur, libelle_couleur FROM couleur'''
+    mycursor.execute(sql)
+    couleurs = mycursor.fetchall()
+    sql = '''SELECT id_taille, libelle_taille FROM taille'''
+    mycursor.execute(sql)
+    tailles = mycursor.fetchall()
+    sql = '''SELECT taille_id, id_declinaison_velo FROM declinaison_velo
+             WHERE id_declinaison_velo = %s'''
+    mycursor.execute(sql, (id_declinaison_velo,))
+    d_taille_uniq = mycursor.fetchone()
+    if d_taille_uniq is not None:
+        taille_id = d_taille_uniq.get('taille_id')
+        if taille_id == 1:
+            d_taille_uniq = 1
+    sql = '''SELECT couleur_id, id_declinaison_velo FROM declinaison_velo
+             WHERE id_declinaison_velo = %s'''
+    mycursor.execute(sql, (id_declinaison_velo,))
+    d_couleur_uniq = mycursor.fetchone()
+    if d_couleur_uniq is not None:
+        couleur_id = d_couleur_uniq.get('couleur_id')
+        if couleur_id == 1:
+            d_couleur_uniq = 1
+    return render_template('admin/velo/edit_declinaison_velo.html', tailles=tailles, couleurs=couleurs, declinaison_velo=declinaison_velo, d_taille_uniq=d_taille_uniq, d_couleur_uniq=d_couleur_uniq)
+
+
 @admin_declinaison_velo.route('/admin/declinaison_velo/edit', methods=['POST'])
 def valid_edit_declinaison_velo():
     mycursor = get_db().cursor()
     id_declinaison_velo = request.form.get('id_declinaison_velo', '')
     id_velo = request.form.get('id_velo', '')
-    stock_declinaison = request.form.get('stock_declinaison', '')
-    taille_id = request.form.get('taille_id', '')
-    couleur_id = request.form.get('couleur_id', '')
+    stock_declinaison = request.form.get('stock', '')
+    taille_id = request.form.get('taille', '')
+    couleur_id = request.form.get('couleur', '')
     tuple_update = (stock_declinaison, id_velo, taille_id, couleur_id, id_declinaison_velo)
-    sql = '''UPDATE declinaison SET stock_declinaison = %s, velo_id = %s, taille_id = %s, couleur_id = %s
+    sql = '''UPDATE declinaison_velo SET stock = %s, velo_id = %s, taille_id = %s, couleur_id = %s
              WHERE id_declinaison_velo = %s'''
     mycursor.execute(sql, tuple_update)
     get_db().commit()
@@ -85,24 +124,30 @@ def valid_edit_declinaison_velo():
     return redirect('/admin/velo/edit?id_velo=' + str(id_velo))
 
 
-@admin_declinaison_velo.route('/admin/declinaison_velo/edit', methods=['POST'])
-def valid_edit_declinaison_velo():
-    id_declinaison_velo = request.form.get('id_declinaison_velo','')
-    id_velo = request.form.get('id_velo','')
-    stock = request.form.get('stock','')
-    taille_id = request.form.get('id_taille','')
-    couleur_id = request.form.get('id_couleur','')
-    mycursor = get_db().cursor()
-
-    message = u'declinaison_velo modifié , id:' + str(id_declinaison_velo) + '- stock :' + str(stock) + ' - taille_id:' + str(taille_id) + ' - couleur_id:' + str(couleur_id)
-    flash(message, 'alert-success')
-    return redirect('/admin/velo/edit?id_velo=' + str(id_velo))
-
-
 @admin_declinaison_velo.route('/admin/declinaison_velo/delete', methods=['GET'])
 def admin_delete_declinaison_velo():
-    id_declinaison_velo = request.args.get('id_declinaison_velo','')
-    id_velo = request.args.get('id_velo','')
+    mycursor = get_db().cursor()
+    id_declinaison_velo = request.args.get('id_declinaison_velo', '')
+    id_velo = request.args.get('id_velo', '')
+    sql = '''SELECT * FROM ligne_panier 
+             WHERE velo_declinaison_id = %s'''
+    mycursor.execute(sql, (id_declinaison_velo,))
+    declinaisons_dans_panier = mycursor.fetchall()
 
-    flash(u'declinaison supprimée, id_declinaison_velo : ' + str(id_declinaison_velo),  'alert-success')
+    sql = '''SELECT * FROM ligne_commande
+             WHERE velo_declinaison_id = %s'''
+    mycursor.execute(sql, (id_declinaison_velo,))
+    declinaisons_dans_commande = mycursor.fetchall()
+
+    if declinaisons_dans_panier:
+        flash('Il y a des exemplaires dans des paniers : vous ne pouvez pas le supprimer', 'alert-warning')
+    elif declinaisons_dans_commande:
+        flash('Il y a des exemplaires dans des commandes : vous ne pouvez pas le supprimer', 'alert-warning')
+    else:
+        sql = '''DELETE FROM declinaison_velo
+                 WHERE id_declinaison_velo = %s AND velo_id = %s'''
+        mycursor.execute(sql, (id_declinaison_velo, id_velo,))
+        get_db().commit()
+        flash(u'Déclinaison supprimée avec succès, id_declinaison_velo : ' + str(id_declinaison_velo), 'alert-success')
+
     return redirect('/admin/velo/edit?id_velo=' + str(id_velo))
